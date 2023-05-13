0ba4d7931dfe0af17527ab1ac3cac8db7baca094
Kirill <k@kazakov.xyz>
2023-05-13 14:46:32 +0300

    Change the main image

--- infra/aws/main.tf
+++ infra/aws/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "web" {
                 sudo yum install -y docker
                 sudo service docker start
                 sudo usermod -a -G docker ec2-user
-                docker run -d -p 80:80 --name nginx nginx
+                docker run -d -p 80:8080 --name deft  kksudo/deft:v.0.2-alpine
                 EOF
 
   tags = var.tags
