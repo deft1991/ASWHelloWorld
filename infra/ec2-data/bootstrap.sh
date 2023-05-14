8b62cbf09baa8a9c7f12dc37a19e39c651c1daa5
Kirill <k@kazakov.xyz>
2023-05-15 01:21:50 +0300

    WIP

--- infra/ec2-data/bootstrap.sh
+++ infra/ec2-data/bootstrap.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 # This file will be executed on the EC2 instance as a bootstrap script. Only first init.
 
-sudo yum update -y
+yum update -y
 # Install docker and docker-compose to ami linux
-sudo yum install -y docker
-sudo curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
-sudo service docker start
-sudo chkconfig docker on
-sudo usermod -a -G docker ec2-user
+yum install -y docker
+curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+service docker start
+chkconfig docker on
+
+# Add a new user for rerun docker apps from CI/CD
+useradd -m cicd-user
+# Generate ssh key for cicd-user
+su - cicd-user -c "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa"
+usermod -a -G docker cicd-user
+# Add cicd-user to ec2-user group to change dir to /home/ec2-user
+usermod -a -G ec2-user cicd-user
+# Allow any user in the group of ec2-user enter to the dir /home/ec2-user
+chmod 750 /home/ec2-user
+
+usermod -a -G docker ec2-user
 cd /home/ec2-user || exit 1
 docker-compose up -d
